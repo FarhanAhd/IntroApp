88a6fd697c4d53423027ac85838c2ca47a46a8af
FarhanAhd <trixiephany@gmail.com>
2024-06-20 17:03:46 +0530

    New Branch - Making HomeActivity the launch screen

--- app/src/main/java/com/example/introapp/HomeActivity.kt
+++ app/src/main/java/com/example/introapp/HomeActivity.kt
@@ -15,20 +15,17 @@ class HomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
         // brake
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+              val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+              v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+              insets
+          }
         //  if(intent.extras != null) {
-        var data = intent.extras?.getString("nkey")
-        Log.i("HomeActivity","data is = "+data)
-        val homeTextView:TextView = findViewById(R.id.tvHome)
-        homeTextView.setText(data)
-
+              var data = intent.extras?.getString("nkey")
+          Log.i("HomeActivity","data is = "+data)
+           val homeTextView:TextView = findViewById(R.id.tvHome)
+           homeTextView.setText(data)
+  */
     }
 
-    override fun onStart() {
-        super.onStart()
-    }
 }
