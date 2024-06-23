d1acacb497e592b55e52ecd226a2d08fd8d79631
FarhanAhd <trixiephany@gmail.com>
2024-06-23 13:29:47 +0530

    roomdb (Querying Data)- Part 3

--- app/src/main/java/com/example/introapp/HomeActivity.kt
+++ app/src/main/java/com/example/introapp/HomeActivity.kt
@@ -2,37 +2,47 @@ package com.example.introapp
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 import androidx.appcompat.app.AppCompatActivity
 
 import com.example.introapp.database.Item
 import com.example.introapp.database.ItemDao
 import com.example.introapp.database.ItemRoomDatabase
+import kotlinx.coroutines.Dispatchers
 
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class HomeActivity : AppCompatActivity() {
     lateinit var dao: ItemDao
+    lateinit var tvHome:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // enableEdgeToEdge()
         setContentView(R.layout.activity_home)
+        tvHome = findViewById(R.id.tvHome)
         var  database = ItemRoomDatabase.getDatabase(this)
         dao = database.itemDao()
 
     }
 
-    override fun onStart() {
-        super.onStart()
-    }
+
 
     fun insertDb(view: View) {
         GlobalScope.launch {
             val item = Item(777,"fruits",111.0,22)
             dao.insert(item)
 
+        }
+    }
+
+    fun findItemDb(view: View) {
+        GlobalScope.launch(Dispatchers.Main) {
+            val item = dao.getItem(777).first()
+            tvHome.setText(item.itemName)
         }
     }
 }
