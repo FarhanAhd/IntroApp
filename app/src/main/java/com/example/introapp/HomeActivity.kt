7527442c2a41f557715bcdd645fed65663b678ad
FarhanAhd <trixiephany@gmail.com>
2024-06-29 21:36:44 +0530

    alarm manager

--- app/src/main/java/com/example/introapp/HomeActivity.kt
+++ app/src/main/java/com/example/introapp/HomeActivity.kt
@@ -2,11 +2,14 @@ package com.example.introapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class HomeActivity : AppCompatActivity() {
     //lateinit var brake:Int?
@@ -14,12 +17,23 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        // brake
 
 
     }
 
     override fun onStart() {
         super.onStart()
+        var view:ConstraintLayout = findViewById(R.id.main)
+        Snackbar.make(view,"sending sms wishes",Snackbar.LENGTH_LONG).setAction("stop sending",
+            View.OnClickListener {  }).show()
+
+        /* val smsManager: SmsManager = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+         this.getSystemService(SmsManager::class.java)
+     } else {
+         SmsManager.getDefault()
+     }*/
+
+        /*    val smsManager:SmsManager = SmsManager.getDefault()
+            smsManager.sendTextMessage("9880979732",null,"happy day",null,null)*/
     }
 }
